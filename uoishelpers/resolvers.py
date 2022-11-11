eaa5184e24e0253dbc5f10f2add4249cec59d59d
Profesor Hrbolek <profesor@hrbolek.cz>
2022-11-11 05:13:19 +0100

    Update

--- uoishelpers/resolvers.py
+++ uoishelpers/resolvers.py
@@ -1,5 +1,9 @@
-from typing import Any, Coroutine, Callable, Awaitable, Union, List
+from optparse import Option
+from types import UnionType
+from typing import Any, Coroutine, Callable, Awaitable, Union, List, Optional
 import uuid
+
+import sqlalchemy
 from sqlalchemy.future import select
 from sqlalchemy.orm import selectinload, joinedload
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -26,6 +30,102 @@ async def putSingleEntityToDb(session, entity):
         session.add(entity)
     await session.commit()
     return entity
+
+
+import strawberry # strawberry-graphql==0.119.0
+def createFilterInputType(rootName, names=[]):
+    assert not(len(names) == 0), "There must be some names"
+    # {'where': {'_or': [{'name': {'_eq': 5}}, {'name': {'_eq': 4}}]}}
+    
+    def createCustomInput(attributeName):
+        result = type(f'{rootName}By_{attributeName}', (object, ), {})
+        result.__annotations__ = dict((op, Optional[str]) for op in ['_eq', '_le', '_lt', '_ge', '_gt'])
+        for op in ['_eq', '_le', '_lt', '_ge', '_gt']:
+            setattr(result, op, None)
+        return strawberry.input(result) # this is decoration
+
+    customInputFilters = dict((name, createCustomInput(name)) for name in names)
+    
+    
+    AllAttributes = type(f'{rootName}AllAttributes', (object, ), {})
+    for name in names:
+        setattr(AllAttributes, name, None)
+    AllAttributes.__annotations__ = dict((name, Optional[customInputFilters[name]]) for name in names)
+    AllAttributes = strawberry.input(AllAttributes)
+    
+    Filter = type(f'{rootName}Filter', (object, ), {})
+    Filter.__annotations__ = {
+        '_or': Optional[List[AllAttributes]],
+        '_and': Optional[List[AllAttributes]],
+        **dict((name, Optional[customInputFilters[name]]) for name in names)
+    }
+    Filter._or = None
+    Filter._and = None
+    for name in names:
+        setattr(Filter, name, None)
+    Filter = strawberry.input(Filter)
+
+    return Filter
+
+def createEntityGetterWithFilter(DBModel: BaseModel):
+
+    stmt = select(DBModel)
+    mapper = sqlalchemy.inspect(DBModel)
+    
+    columnTypes = dict((item.columns[0].type, item.columns[0].name) for item in mapper.column_attrs)
+
+    def createOrLambda(query):
+        pass
+
+    def createNamedLambda(query):
+        # for queryName, queryValue in query.items():
+        #    break # decomposition, hack, get first key and its value
+
+        queryName = None
+        for key, value in columnTypes.items():
+            if hasattr(query, key):
+                queryName = key
+                break
+
+        methodMaps = {
+            '_eq': '__eq__',
+            '_gt': '__gt__',
+            '_lt': '__lt__',
+            '_ge': '__ge__',
+            '_le': '__le__',
+        }
+
+        foundValue = None
+        if not(queryName is None):
+            foundValue = getattr(query, queryName)
+
+            comparedItem = None
+            for key, value in methodMaps.items():
+                if hasattr(foundValue, key):
+                    comparedItem = value
+                    break
+
+            return getattr(DBModel, methodMaps[key])(comparedItem)
+
+        return (DBModel.id == DBModel.id) # will this work?
+
+
+        
+
+
+
+    def createWhereLambda(where):        
+        pass
+
+    async def FullResolver(session, skip: Optional[int] = 0, limit: Optional[int] = 10, where = None) -> List[DBModel]:
+        stmtWithFilter = stmt.offset(skip).limit(limit)
+        dbSet = await session.execute(stmtWithFilter)
+        result = dbSet.scalars()
+        return result
+
+    return FullResolver
+
+
 
 def createEntityGetter(DBModel: BaseModel, options=None) -> Callable[[AsyncSession, int, int], Awaitable[Union[BaseModel, None]]]:
     """Předkonfiguruje dotaz do databáze na vektor entit
@@ -187,7 +287,7 @@ def createUpdateResolver(DBModel: BaseModel) -> Callable[[AsyncSession, uuid.UUI
     Callable[[session, id, data], awaitable]
         async function for update
     """
-    async def resolveUpdate(session: AsyncSession, id: uuid.UUID, data: dict) -> Awaitable[DBModel]:
+    async def resolveUpdate(session: AsyncSession, id: uuid.UUID, data: dict, extraAttributes={}) -> Awaitable[DBModel]:
         """Updates a record with id=id according give data
 
         Parameters
@@ -207,7 +307,7 @@ def createUpdateResolver(DBModel: BaseModel) -> Callable[[AsyncSession, uuid.UUI
         stmt = select(DBModel).filter_by(id=id)
         dbSet = await session.execute(stmt)
         dbRecord = dbSet.scalars().first()
-        result = update(dbRecord, data)
+        result = update(dbRecord, data, extraAttributes)
         await session.commit()
         return result
     return resolveUpdate
@@ -225,7 +325,7 @@ def createInsertResolver(DBModel: BaseModel) -> Callable[[AsyncSession, BaseMode
     Callable[[session, id, data], awaitable]
         async function for update
     """
-    async def resolveInsert(session, data, extraAttributes) -> Awaitable[DBModel]:
+    async def resolveInsert(session, data, extraAttributes={}) -> Awaitable[DBModel]:
         """Inserts a new record into database with given data
 
         Parameters
@@ -242,6 +342,6 @@ def createInsertResolver(DBModel: BaseModel) -> Callable[[AsyncSession, BaseMode
             datastructure saved in database
         """
         dbRecord = DBModel()
-        result = await putSingleEntityToDb(session, update(dbRecord, data, extraAttributes=extraAttributes))
+        result = await putSingleEntityToDb(session, update(dbRecord, data, extraAttributes))
         return result
     return resolveInsert
