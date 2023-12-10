c7eb78cea3e1acff15708e2dbfd3a629583f4d52
Hrbolek <alexandr.stefek@unob.cz>
2023-12-10 18:41:22 +0100

    added createAuthentizationSentinel

--- uoishelpers/authenticationMiddleware.py
+++ uoishelpers/authenticationMiddleware.py
@@ -1,5 +1,5 @@
 import os
-
+from typing import Any
 from starlette.authentication import (
     AuthCredentials, AuthenticationBackend, AuthenticationError
 )
@@ -125,3 +125,136 @@ class BasicAuthenticationMiddleware404(AuthenticationMiddleware):
     def default_on_error(conn: HTTPConnection, exc: Exception) -> Response:
         where = conn.url.path
         return PlainTextResponse(f"Unauthorized for {where}", status_code=404)
+
+from pydantic import BaseModel
+import aiohttp
+import json
+import jwt
+from fastapi import Request
+from starlette.responses import JSONResponse
+
+class AuthenticationError(Exception):
+    pass
+
+class Item(BaseModel):
+    query: str
+    variables: dict = None
+    operationName: str = None
+
+apolloQuery = "query __ApolloGetServiceDefinition__ { _service { sdl } }"
+graphiQLQuery = "\n query IntrospectionQuery {\n __schema {\n \n queryType { name }\n mutationType { name }\n subscriptionType { name }\n types {\n ...FullType\n }\n directives {\n name\n description\n \n locations\n args(includeDeprecated: true) {\n ...InputValue\n }\n }\n }\n }\n\n fragment FullType on __Type {\n kind\n name\n description\n \n fields…name\n ofType {\n kind\n name\n ofType {\n kind\n name\n ofType {\n kind\n name\n ofType {\n kind\n name\n ofType {\n kind\n name\n ofType {\n kind\n name\n }\n }\n }\n }\n }\n }\n }\n }\n "
+JWTPUBLICKEYURL = "http://localhost:8000/oauth/publickey"
+JWTRESOLVEUSERPATHURL = "http://localhost:8000/oauth/userinfo"
+
+def createAuthentizationSentinel(
+        queriesWOAuthentization = [apolloQuery, graphiQLQuery],
+        JWTPUBLICKEY = JWTPUBLICKEYURL,
+        JWTRESOLVEUSERPATH = JWTRESOLVEUSERPATHURL,
+        onAuthenticationError = lambda item: JSONResponse(f"{item} unauthorized")
+):
+    class Sentinel:
+        def __init__(self,
+            JWTPUBLICKEY = JWTPUBLICKEY,
+            JWTRESOLVEUSERPATH = JWTRESOLVEUSERPATH):
+            self.JWTPUBLICKEY = JWTPUBLICKEY
+            self.JWTRESOLVEUSERPATH = JWTRESOLVEUSERPATH
+
+        async def __call__(self, request: Request, item: Item) -> Any:
+            if item.query in queriesWOAuthentization:
+                logging.info(f"free access to {item.query}")
+                return None
+            try:
+                await self.authenticate(request=request)
+            except:
+                logging.info(f"unauthorized access to {item.query}")
+                return onAuthenticationError(item) 
+            pass
+
+        async def getPublicKey(self):
+            async with aiohttp.ClientSession() as session:
+                async with session.get(self.JWTPUBLICKEY) as resp:
+                    print(resp.status)
+                    if resp.status != 200:
+                        raise AuthenticationError("Public key not available")
+
+                    # publickey = await resp.read()
+                    publickey = await resp.text()
+            self.publickey = publickey.replace('"', '').replace('\\n', '\n')
+            print('got key', self.publickey)
+            self.publickey = self.publickey.encode()
+            return self.publickey
+
+        async def authenticate(self, request: Request):
+            print("# BEGIN #######################################")
+            client = request.client
+            headers = request.headers
+            cookies = request.cookies
+            url = request.url
+            base_url = request.base_url
+            uri = url.path
+            request.url.path
+            logging.info(f'{base_url} {client}, {headers}, {cookies}')
+            logging.info(f'{uri}')
+            print(f'{base_url} {client}, {headers}, {cookies}')
+            print(f'{uri}')        
+            
+            # 1. ziskat jwt (cookies authorization nebo header Authorization: Bearer )
+            jwtsource = cookies.get("authorization", None)
+            if jwtsource is None:
+                jwtsource = headers.get("Authorization", None)
+                if jwtsource is not None:
+                    [_, jwtsource] = jwtsource.split("Bearer ")
+                else:
+                    #unathorized
+                    pass
+
+            print('got jwtsource', jwtsource)
+            if jwtsource is None:
+                raise AuthenticationError("missing code")
+
+            # 2. ziskat verejny klic (async request to authority)
+            publickey = self.publickey
+            if publickey is None:
+                publickey = await self.getPublicKey()
+            
+            # 3. overit jwt (lokalne)
+            for i in range(2):
+                try:
+                    jwtdecoded = jwt.decode(jwt=jwtsource, key=publickey, algorithms=["RS256"])
+                    break
+                except jwt.InvalidSignatureError as e:
+                    # je mozne ulozit key do cache a pri chybe si key ziskat (obnovit) a provest revalidaci
+                    print(e)
+                if (i == 1):
+                    # klic byl aktualizovan a presto doslo k vyjimce
+                    raise AuthenticationError("Invalid signature")
+                
+                # aktualizace klice, predchozi selhal
+                publickey = await self.getPublicKey()
+                print('publickey refreshed', publickey)
+            
+            print('got jwtdecoded', jwtdecoded)
+
+            # 3A. pokud jwt obsahuje user.id, vzit jej primo
+            user_id = jwtdecoded.get("user_id", None)
+            print("some user?", user_id)
+
+            # 4. pouzit jwt jako parametr pro identifikaci uzivatele u autority
+            if user_id is None:
+                async with aiohttp.ClientSession() as session:
+                    headers = {"Authorization": f"Bearer {jwtdecoded['access_token']}"}
+                    async with session.get(self.JWTRESOLVEUSERPATH, headers=headers) as resp:
+                        print(resp.status)
+                        assert resp.status == 200
+                        userinfo = await resp.json()
+                        print("got userinfo", userinfo)
+                        print("got userinfo", userinfo["user"])
+
+            # demouser = os.getenv("DEMOUSER", '{"id": "2d9dc5ca-a4a2-11ed-b9df-0242ac120003", "name": "John", "surname": "Newbie"}')
+            # user = json.loads(demouser)
+            # if user_id is None:
+            #     user["id"] = user_id
+                
+            request.scope["user"] = {"id": user_id}
+            print("# SUCCESS #######################################")
+    return Sentinel()
