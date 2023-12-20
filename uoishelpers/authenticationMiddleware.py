cfff9b8ed46f5447b2ce0e658d84747a59737045
Hrbolek <alexandr.stefek@unob.cz>
2023-12-20 19:27:33 +0100

    sentinel improved

--- uoishelpers/authenticationMiddleware.py
+++ uoishelpers/authenticationMiddleware.py
@@ -46,8 +46,8 @@ class BasicAuthBackend(AuthenticationBackend):
         base_url = conn.base_url
         uri = url.path
         conn.url.path
-        logging.info(f'{base_url} {client}, {headers}, {cookies}')
-        logging.info(f'{uri}')
+        logging.debug(f'{base_url} {client}, {headers}, {cookies}')
+        logging.debug(f'{uri}')
         print(f'{base_url} {client}, {headers}, {cookies}')
         print(f'{uri}')        
         
@@ -161,29 +161,29 @@ def createAuthentizationSentinel(
             self.publickey = None
 
         async def __call__(self, request: Request, item: Item) -> Any:
-            if item.query in queriesWOAuthentization:
-                logging.info(f"Sentinel advice: this is free access to \n {item.query}")
-                return None
             try:
                 await self.authenticate(request=request)
             except:
+                if item.query in queriesWOAuthentization:
+                    logging.info(f"Sentinel advice: this is free access to \n {item.query}")
+                    return None
                 logging.info(f"Sentinel advice: unauthorized access to \n {item.query}")
                 return onAuthenticationError(item) 
             logging.info(f"Sentinel advice: ok access to \n {item.query}")
             pass
 
         async def getPublicKey(self):
-            logging.info(f"Sentinel getting public key on \n {self.JWTPUBLICKEY}")
+            logging.debug(f"Sentinel getting public key on \n {self.JWTPUBLICKEY}")
             async with aiohttp.ClientSession() as session:
                 async with session.get(self.JWTPUBLICKEY) as resp:
                     print(resp.status)
-                    logging.info(f"response from oauth authority: status {resp}")
+                    logging.debug(f"response from oauth authority: status {resp}")
                     if resp.status != 200:
                         raise AuthenticationError("Public key not available")
 
                     # publickey = await resp.read()
                     publickey = await resp.text()
-                    logging.info(f"Sentinel has got public key \n {publickey}")
+                    logging.debug(f"Sentinel has got public key \n {publickey}")
             self.publickey = publickey.replace('"', '').replace('\\n', '\n')
             print('got key', self.publickey)
             self.publickey = self.publickey.encode()
@@ -198,12 +198,12 @@ def createAuthentizationSentinel(
             base_url = request.base_url
             uri = url.path
             request.url.path
-            logging.info(f'Sentinel authentication phase message: \n {base_url} {client}, {headers}, {cookies}')
-            logging.info(f'{uri}')
+            logging.debug(f'Sentinel authentication phase message: \n {base_url} {client}, {headers}, {cookies}')
+            logging.debug(f'{uri}')
             print(f'{base_url} {client}, {headers}, {cookies}')
             print(f'{uri}')        
             
-            logging.info(f'1. ziskat jwt (cookies authorization nebo header Authorization: Bearer )')
+            logging.debug(f'1. ziskat jwt (cookies authorization nebo header Authorization: Bearer )')
             # 1. ziskat jwt (cookies authorization nebo header Authorization: Bearer )
             jwtsource = cookies.get("authorization", None)
             if jwtsource is None:
@@ -213,20 +213,20 @@ def createAuthentizationSentinel(
                 else:
                     #unathorized
                     pass
-            logging.info(f'Sentinel authentication phase message: token: \n {jwtsource}')
+            logging.debug(f'Sentinel authentication phase message: token: \n {jwtsource}')
             # print('Sentinel got jwtsource', jwtsource, "\n", self.publickey)
-            logging.info(30*"#")
+            logging.debug(30*"#")
             if jwtsource is None:
-                logging.info(f'Sentinel authentication phase message: TOKEN IS MISSING')
+                logging.debug(f'Sentinel authentication phase message: TOKEN IS MISSING')
                 raise AuthenticationError("missing code")
 
             # 2. ziskat verejny klic (async request to authority)
-            logging.info("2. ziskat verejny klic (async request to authority)")
+            logging.debug("2. ziskat verejny klic (async request to authority)")
             publickey = self.publickey
             if publickey is None:
                 publickey = await self.getPublicKey()
-            logging.info(30*"#")
-            logging.info(f'have public key')
+            logging.debug(30*"#")
+            logging.debug(f'have public key')
             print(f'have public key')
 
             # 3. overit jwt (lokalne)
@@ -246,23 +246,23 @@ def createAuthentizationSentinel(
                 print('publickey refreshed', publickey)
             
             print('got jwtdecoded', jwtdecoded)
-            logging.info(f'got jwtdecoded {jwtdecoded}')
+            logging.debug(f'got jwtdecoded {jwtdecoded}')
             # 3A. pokud jwt obsahuje user.id, vzit jej primo
-            logging.info("3A. pokud jwt obsahuje user.id, vzit jej primo")
+            logging.debug("3A. pokud jwt obsahuje user.id, vzit jej primo")
             user_id = jwtdecoded.get("user_id", None)
             print("some user?", user_id)
 
             # 4. pouzit jwt jako parametr pro identifikaci uzivatele u autority
             if user_id is None:
-                logging.info(f"4. pouzit jwt jako parametr pro identifikaci uzivatele u autority {self.JWTRESOLVEUSERPATH}")
+                logging.debug(f"4. pouzit jwt jako parametr pro identifikaci uzivatele u autority {self.JWTRESOLVEUSERPATH}")
                 async with aiohttp.ClientSession() as session:
                     headers = {"Authorization": f"Bearer {jwtdecoded['access_token']}"}
                     async with session.get(self.JWTRESOLVEUSERPATH, headers=headers) as resp:
-                        logging.info(f"Autority response {resp}")
+                        logging.debug(f"Autority response {resp}")
                         print(resp.status)
                         assert resp.status == 200
                         userinfo = await resp.json()
-                        logging.info(f"Autority response user is {userinfo}")
+                        logging.debug(f"Autority response user is {userinfo}")
                         print("got userinfo", userinfo)
                         user_id = userinfo["id"]
 
@@ -271,11 +271,11 @@ def createAuthentizationSentinel(
             # if user_id is None:
             #     user["id"] = user_id
                 
-            logging.info(f"We know that user is {user_id}")
+            logging.debug(f"We know that user is {user_id}")
             try:
                 request.scope["user"] = {"id": user_id}
             except Exception as e:
-                logging.info(f"WTF {e}")
+                logging.debug(f"WTF {e}")
             print("# SUCCESS #######################################")
             if user_id is None:
                 raise AuthenticationError(f"Unknown user")
