8efe304a93fe855c8c7ef37894547c2c04728aa0
Hrbolek <alexandr.stefek@unob.cz>
2024-11-25 11:27:50 +0100

    move to cfg

--- setup.py
+++ setup.py
@@ -1,23 +1,3 @@
 from setuptools import setup
 
-setup(
-      name='uoishelpers',
-
-      version='0.3.1',
-      description='Library for _uois project.',
-      url='http://github.com/hrbolek/uoishelpers',
-      author='Profesor Hrbolek',
-      author_email='profesor@hrbolek.cz',
-      license='MIT',
-      packages=['uoishelpers'],
-      install_requires=[
-          ''
-      ],      
-      classifiers=[
-        'Development Status :: 3 - Alpha',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Topic :: Database ',
-      ],
-      zip_safe=False)
+setup()
