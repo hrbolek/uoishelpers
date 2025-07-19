9e454a1a847245089e8661efe6fcb25e47af73c3
Hrbolek <alexandr.stefek@unob.cz>
2025-07-19 11:47:53 +0200

    name corrected

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     entry_points={
         'console_scripts': [
-            'listtypes = uoishelpers.cmds.listypes:listypes',
+            'listtypes = uoishelpers.cmds.listtypes:listtypes',
         ],
     },
 
